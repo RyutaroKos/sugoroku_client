55907c24d3bdd889b153930b60b8156955ca96e5
SergeFan <sergefan@live.com>
2021-12-12 09:49:19 +0900

    Branch test

--- app/src/main/java/com/example/app/App.java
+++ app/src/main/java/com/example/app/App.java
@@ -23,7 +23,6 @@ public class App {
 
         MainFrame.createFrame(appTitle);
 
-        System.out.println("This is the test branch.");
-        System.out.println("This is the test content.");
+        System.out.println("This is the main branch.");
     }
 }
