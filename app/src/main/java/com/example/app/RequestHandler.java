84e47eb51fc7282797c0a41b955b06bd4dc3d587
SergeFan <sergefan@live.com>
2021-12-26 02:07:56 +0900

    Fix request & result handler exception handling

--- app/src/main/java/com/example/app/RequestHandler.java
+++ app/src/main/java/com/example/app/RequestHandler.java
@@ -16,16 +16,16 @@ public class RequestHandler implements Runnable {
     @Override
     public void run() {
         while (true) {
-            if (webSocketClient.getConnectionStatus() && !RequestBuffer.getInstance().isEmpty()) {
-                JSONObject requestObject = RequestBuffer.getInstance().retrieveRequest();
-                webSocketClient.sendRequest(requestObject.toString());
-                System.out.println(requestObject);
-            } else {
-                try {
+            try {
+                if (webSocketClient.getConnectionStatus() && !RequestBuffer.getInstance().isEmpty()) {
+                    JSONObject requestObject = RequestBuffer.getInstance().retrieveRequest();
+                    webSocketClient.sendRequest(requestObject.toString());
+                    System.out.println(requestObject);
+                } else {
                     TimeUnit.MICROSECONDS.sleep(100); //request読み取り処理が追いつかない場合、このタイムアウトを延長
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
                 }
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
     }
